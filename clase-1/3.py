ca9b9ec0a2c34e5e1b010ca63c94269b4fb9ee7d
Emir <emirpolitog@gmail.com>
2026-05-27 16:06:12 -0600

    Ejercicios de la primera clase principios de python

--- clase-1/3.py
+++ clase-1/3.py
@@ -1,8 +1,9 @@
-# Declarar, pedir y mostrar
+#1.- Declarar, pedir y mostrar
 # nombre = input("Escribe tu nombre:")
 # print("Hola", nombre)
 
-#
+#2.- Declarar, pedir y mostrar
 edad= input("Escribe tu edad:")
 edad= int(edad)+1
 print(edad)
+
---
+++ clase-1/4.py
@@ -0,0 +1,9 @@
+# Operadores
+a = 10
+b = 5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a ** b)
